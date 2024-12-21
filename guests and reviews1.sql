c7ec4175eaa2863aa1a072150a79186b1d0bc005
Salma-Amr53 <s.amr2447@nu.edu.eg>
2024-12-21 20:33:40 +0200

    Update guests and reviews1.sql

--- guests and reviews1.sql
+++ guests and reviews1.sql
@@ -3,10 +3,9 @@ USE hotel;
 CREATE TABLE Guests (
     GuestID INT PRIMARY KEY AUTO_INCREMENT,
     FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50),
     Email VARCHAR(100) UNIQUE NOT NULL,
     PhoneNumber VARCHAR(15),
-    Address TEXT,
     CheckInDate DATE NOT NULL,
     CheckOutDate DATE
 );
@@ -15,7 +14,7 @@ CREATE TABLE HotelReviews (
     ReviewID INT PRIMARY KEY AUTO_INCREMENT,
     GuestID INT NOT NULL,
     ReviewDate DATE NOT NULL,
-    Rating INT CHECK (Rating BETWEEN 1 AND 5),
+    Rating INT CHECK (Rating BETWEEN 1 AND 10),
     Comments TEXT,
     FOREIGN KEY (GuestID) REFERENCES Guests(GuestID) ON DELETE CASCADE
 );
